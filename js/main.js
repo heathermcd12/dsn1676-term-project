a1d445aba6314f6b607c3301f00d8703892580c5
heathermcd12 <heather-mcdonell@hotmail.com>
2015-03-15 21:45:24 -0400

    Jquery transition

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var $btnShowHide = $('.btn-show-hide');
-var $text = $('.box');
+var $text = $('.text');
 
-console.log ($box);
+console.log ($text);
 
 $btnShowHide.on('click', function () { //'click' is the action we want the user to do, then the function of rotation will occur
     $text.toggleClass('js-box-show-hide'); //adding a new class, TOGGLE: switch class on and off
