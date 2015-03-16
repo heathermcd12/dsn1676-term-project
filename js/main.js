3b747d21b8b8969b63cf05536161ebe72cf27717
heathermcd12 <heather-mcdonell@hotmail.com>
2015-03-15 23:51:17 -0400

    portrait
    having difficulty trying to add a grid to the portrait class

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 var $btnExpand = $('.btn-expand');
 var $panel = $('.panel');
 
+var $btnMove = $('.btn-move');
+var $portrait = $('.portrait');
+
 console.log($panel);
+console.log($portrait);
 
 $btnExpand.on ('click', function () {
     $panel.toggleClass('js-panel-expand');
