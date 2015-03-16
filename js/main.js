fe7bae9bb21e95e3e257ef94c2decac5899687b9
heathermcd12 <heather-mcdonell@hotmail.com>
2015-03-16 19:06:30 -0400

    addition of photos, javascript and css

--- js/main.js
+++ js/main.js
@@ -1,17 +1,28 @@
 var $btnExpand = $('.btn-expand');
 var $panel = $('.panel');
 
-var $btnShowHide = $('.btn-show-hide');
-var $box = $('.box');
+var $btnShowHide1 = $('.btn-show-hide-1');
+var $box1 = $('.box-1');
 
-console.log($panel);
-console.log($box);
+var $btnShowHide2 = $('.btn-show-hide-2');
+var $box2 = $('.box-2');
+
+var $btnShowHide3 = $('.btn-show-hide-3');
+var $box3 = $('.box-3');
 
 $btnExpand.on ('click', function () {
     $panel.toggleClass('js-panel-expand');
 });
 
-$btnShowHide.on ('click', function () {
-    $box.toggleClass('js-box-show');
+$btnShowHide1.on ('click', function () {
+    $box1.toggleClass('js-box-show');
+});
+
+$btnShowHide2.on ('click', function () {
+    $box2.toggleClass('js-box-show');
+});
+
+$btnShowHide3.on ('click', function () {
+    $box3.toggleClass('js-box-show');
 });
 
