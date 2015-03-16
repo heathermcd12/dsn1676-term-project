5ba2dcb7306ee1952260eb32611b6bf8d70b4faf
heathermcd12 <heather-mcdonell@hotmail.com>
2015-03-16 15:42:03 -0400

    about me

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 var $btnExpand = $('.btn-expand');
 var $panel = $('.panel');
 
-var $btnMove = $('.btn-move');
-var $portrait = $('.portrait');
+var $btnShowHide = $('.btn-show-hide');
+var $box = $('.box');
 
 console.log($panel);
-console.log($portrait);
+console.log($box);
 
 $btnExpand.on ('click', function () {
     $panel.toggleClass('js-panel-expand');
 });
+
+$btnShowHide.on ('click', function () {
+    $box.toggleClass('js-box-show');
+});
+
